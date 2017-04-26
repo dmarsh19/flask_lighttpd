3b8a3fdc01e7e52089ddeb4351fc07ae9d3b7fb3
Derek Marsh <derekm224@gmail.com>
2017-04-26 14:32:04 -0400

    Initial setup. Used existing homeportal  template.

---
+++ settings.py
@@ -0,0 +1,10 @@
+SECRET_KEY = 'aabbccdd11223344'
+DEBUG = True
+
+#SERVER_NAME = 'ec2-52-90-156-155.compute-1.amazonaws.com'
+#SERVER_NAME = '10.12.24.11'
+PREFERRED_URL_SCHEME = 'https'
+
+# this script is imported. Better to hardcode a path here or write a log
+# to a created folder in /var/log
+APP_LOGFILE = '/home/dmarsh/dev/flask_lighttpd_project/flask_lighttpd.log'
